9ee2c7f88b57daf54a2100cad565d27046cbddcc
manishsahu001 <manishsahu.w22@gmail.com>
2026-07-19 20:46:33 +0530

    fixed the issue

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_BASE = import.meta.env.VITE_API_URL || "http://localhost:8000";
+const API_BASE =
+  import.meta.env.VITE_API_URL || "https://legitiq-be.onrender.com";
 
 const api = axios.create({
   baseURL: API_BASE,
